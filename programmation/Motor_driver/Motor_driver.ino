e2c8512732d9c2147a2b94f03a6519dbf952b0c7
jordan morra-taroudjit <m-t.jordan@live.fr>
2016-05-01 14:35:52 +0200

    code commande moteur+correction pb carcteres+utilisation const char

--- programmation/Motor_driver/Motor_driver.ino
+++ programmation/Motor_driver/Motor_driver.ino
@@ -1,43 +1,43 @@
-const char ena = 3; /*simple enable ou variation de vitesse si PWM*/
-const char in1 = 2;  /*broche de contrle du sens de rotation moteur et commande marche arret */
-const char in2 = 4;
-const char potar = 0; /* la broche pour régler la vitesse en tant que test*/
+const char ena=3;/*simple enable ou variation de vitesse si PWM*/
+const char in1=2;/*broche de contrle du sens de rotation moteur et commande marche arret */
+const char in2=4;
+/*const int potar = 0;*/ /* la broche pour régler la vitesse en tant que test*/
 
 
 void setup() {
-  // put your setup code here, to run once:
-
-    pinMode(ena, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
+ 
+   pinMode(ena,OUTPUT);
+   pinMode(in1,OUTPUT);
+   pinMode(in2,OUTPUT);
   // on met le moteur a l'arret
-    analogWrite(ena, 0);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-
+   analogWrite(ena,0);
+   digitalWrite(in1,LOW);
+   digitalWrite(in2,LOW);
 }
 
-void motor_brake( const char ena, const char in1, const char in2, const char pot) {
+void motor_brake_cmd( const char ena, const char in1, const char in2)/*, const char pot)*/ {
   
-  int vitesse = analogRead(potar)/4;
-  analogWrite(ena,vitesse);
+  //int vitesse = analogRead(potar)/4;
+ // analogWrite(ena,vitesse);
+  digitalWrite(ena,HIGH);
   digitalWrite(in1,HIGH);
   digitalWrite(in2,LOW);
   // pour la phase de test
   delay(10000);
 }
 
-void motor_loose( const char ena, const char in1, const char in2, const char pot) {
+void motor_loose_cmd( const char ena, const char in1, const char in2)/*, const char pot)*/ {
   
-  int vitesse = analogRead(potar)/4;
-  analogWrite(ena,vitesse);
+  //int vitesse = analogRead(potar)/4;
+  //analogWrite(ena,vitesse);
+  digitalWrite(ena,HIGH);
   digitalWrite(in1,LOW);
   digitalWrite(in2,HIGH);
   // pour la phase de test
   delay(10000);
 }
 
-void motor_stop(const char ena, const char in1, const char in2){
+void motor_stop_cmd(const char ena, const char in1, const char in2){
   
   analogWrite(ena,0);
   digitalWrite(in1,LOW);
@@ -57,4 +57,8 @@ void motor_decel(const char ena, const char in1, const char in2, float t, float 
 
 void loop(){
   // sequence de test  ecrire
+  motor_brake_cmd(ena,in1,in2);
+  motor_stop_cmd(ena,in1,in2);
+  motor_loose_cmd(ena,in1,in2);
+  
 }
